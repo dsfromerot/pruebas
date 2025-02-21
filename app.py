5350c24b9fb36aa4863ad90711675c83b0ade2e1
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-21 17:29:00 +0100

    Update and rename __init__.py to app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+pruebas = [
+    {"fecha_radiografia": "2023-10-01", "informe": "Fractura", "nombre_doctor": "Dr. Smith"},
+    {"fecha_radiografia": "2023-10-02", "informe": "Tumor", "nombre_doctor": "Dr. Johnson"}
+]
+
+@app.route('/pruebas', methods=['GET'])
+def get_pruebas():
+    return jsonify(pruebas)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
