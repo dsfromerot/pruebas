d04652680219d5f6d9710a1c3646b6ab741c39f2
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-25 04:23:16 -0500

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_oidc import OpenIDConnect
+
+app = Flask(__name__)
+app.config.update({
+    'SECRET_KEY': 'secret',
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
+    'OIDC_USER_INFO_ENABLED': True,
+    'OIDC_SCOPES': ['openid', 'email', 'profile'],
+    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
+})
+oidc = OpenIDConnect(app)
