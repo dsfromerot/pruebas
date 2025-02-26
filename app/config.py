84b441a58f59e49ec02ced85cbde8e0e42189628
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-26 12:54:36 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,22 +2,23 @@ from flask import Flask
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
+
+# Configuración de Flask y Keycloak
 app.config.update({
     'SECRET_KEY': 'secret',
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_CLIENT_SECRETS': '/app/client_secrets.json',  # Ruta absoluta
     'OIDC_ID_TOKEN_COOKIE_SECURE': False,
     'OIDC_REQUIRE_VERIFIED_EMAIL': False,
     'OIDC_USER_INFO_ENABLED': True,
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
 })
+
 oidc = OpenIDConnect(app)
 
-# Ruta de prueba
-@app.route('/')
-def hello():
-    return "¡El microservicio de pruebas está en ejecución!"
+# Importar rutas después de inicializar Flask para evitar errores de importación circular
+from app import routes  
 
-# Punto de entrada para ejecutar la aplicación
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+# Gunicorn busca esta función para iniciar Flask
+def start():
+    return app
